08acb04dbe3a9d6abff629f487d299bb6f61b475
Jacob Tucker <tsnakejake@aol.com>
2022-10-04 20:27:14 -0500

    fix fcl version

--- flow/config.js
+++ flow/config.js
@@ -1,5 +1,7 @@
 import { config } from "@onflow/fcl";
 
 config()
-  .put("accessNode.api", "https://testnet.onflow.org") // This connects us to Flow TestNet
+  .put("accessNode.api", "https://rest-testnet.onflow.org") // This connects us to Flow TestNet
   .put("discovery.wallet", "https://fcl-discovery.onflow.org/testnet/authn/") // Allows us to connect to Blocto & Lilico Wallet
+  .put("app.detail.title", "Beginner Emerald DApp")
+  .put("app.detail.icon", "https://i.imgur.com/ux3lYB9.png")
